d44de823307cf9098c917590329f2b45692f144f
yago1994 <yagoaral@hotmail.com>
2025-11-13 23:44:26 -0500

    Initial Commit

---
+++ semai/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  semai
+//
+//  Created by Yago Arconada on 11/13/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
